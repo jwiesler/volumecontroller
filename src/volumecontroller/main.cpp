70cead19250cd6151a2005c6deb4c590d7e0e38e
Julian Wiesler <wiesleju@gmail.com>
2020-07-13 15:17:09 +0200

    Logging to file

--- src/volumecontroller/main.cpp
+++ src/volumecontroller/main.cpp
@@ -5,12 +5,44 @@
 #include <QScreen>
 #include <QDebug>
 #include <QPalette>
+#include <QFile>
+#include <QMutex>
+
+static QFile logFile("VolumeController.log");
+static QtMessageHandler defaultMessageHandler;
+
+void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+	static QMutex logMutex;
+	QMutexLocker lock(&logMutex);
+
+	defaultMessageHandler(type, context, msg);
+
+	if(logFile.isOpen()) {
+		logFile.write(qFormatLogMessage(type, context, msg).toUtf8() + '\n');
+		logFile.flush();
+	}
+}
 
 int main(int argc, char *argv[])
 {
 	RunGuard guard("volumecontroller");
 	if(!guard.tryToRun())
 		return 0;
+
+	QByteArray envVar = qgetenv("QTDIR");       //  check if the app is ran in Qt Creator
+
+//	if (envVar.isEmpty())
+		logFile.open(QIODevice::Append | QIODevice::Text);
+
+	qSetMessagePattern("%{time MM-dd hh:mm:ss:zzz} %{type} %{threadid} %{function}: %{message}");
+	defaultMessageHandler = qInstallMessageHandler(messageHandler);
+
+	qInfo() << "Starting VolumeController";
+	if(logFile.isOpen()) {
+		qDebug() << "Logging to file and console";
+	} else {
+		qDebug() << "Logging to console";
+	}
 
 	QApplication a(argc, argv);
 	VolumeController w;
